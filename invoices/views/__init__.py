60cab5071f2bda15b8572180c68c904c11416a92
cyberfox947 <comandajuly@gmail.com>
2021-09-18 12:38:40 -0700

    Initial commit

---
+++ invoices/views/__init__.py
@@ -0,0 +1,7 @@
+from invoices.views.invoices import *
+from invoices.views.items import *
+from invoices.views.customers import *
+from invoices.views.expenses import *
+from invoices.views.reports import *
+from invoices.views.userauth import *
+from invoices.views.admin import *
