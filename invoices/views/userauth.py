71a951e29eceff960582fb9a0f7527912565c6d7
cyberfox947 <comandajuly@gmail.com>
2021-09-18 17:36:23 -0700

    6

--- invoices/views/userauth.py
+++ invoices/views/userauth.py
@@ -4,27 +4,25 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 
 
-
 # User login
 def login_view(request):
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(username=username, password=password)
-		if (user is not None):
-			login(request, user)
-			return HttpResponseRedirect(reverse('index'))
-		else:
-			context = {
-				'error_message' : 'Unable to login! Please check username and password then try again.',
-				}
-			return render(request, 'login.html', context)
-	else:
-		return render(request, 'login.html')
-
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            context = {
+                'error_message': 'Unable to login! Please check username and password then try again.',
+            }
+            return render(request, 'login.html', context)
+    else:
+        return render(request, 'login.html')
 
 
 # User logout
 def logout_view(request):
-	logout(request)
-	return HttpResponseRedirect('/')
+    logout(request)
+    return HttpResponseRedirect('/')
